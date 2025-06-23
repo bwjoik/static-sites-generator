71bfc6c79734940e396b0339c9801bff69c0e0b6
Aaronfeng <Law09141311@gmail.com>
2025-06-23 16:15:44 +0800

    final

--- src/gen_page.py
+++ src/gen_page.py
@@ -6,7 +6,7 @@ def extract_title(markdown):
             return line.strip()[2:].strip()
     raise Exception("No h1 header found in markdown")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath = "/"):
     print(f"Generating page from {from_path} using {template_path} to {dest_path}")
     # 讀取 markdown 檔案
     with open(from_path, "r") as f:
@@ -20,11 +20,14 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown)
     # 替換 template 變數
     html_with_placeholders = template.replace("{{ Title }}", title).replace("{{ Content }}", html_string)
+    # 這裡進行 href 與 src 的替換
+    html_with_placeholders = html_with_placeholders.replace('href="/', f'href="{basepath}')
+    html_with_placeholders = html_with_placeholders.replace('src="/', f'src="{basepath}')
     print(f"Generated page: {dest_path}")
     with open(dest_path, "w") as f:
         f.write(html_with_placeholders)
         
-def generate_pages_recursive(dir_path, template_path, dest_dir):
+def generate_pages_recursive(dir_path, template_path, dest_dir, basepath = "/"):
     for filename in os.listdir(dir_path):
         src_path = os.path.join(dir_path, filename)
         dest_path = os.path.join(dest_dir, filename)
--- src/main.py
+++ src/main.py
@@ -4,16 +4,19 @@ import shutil
 import os
 from copytree import copytree
 from gen_page import *
+import sys
 print("hello world")
 
 def main():
-    #delete file in public folder
-    for filename in os.listdir('public'):
-        filepath = os.path.join('public', filename)
-    #copy static files to public folder
-    copytree("static","public")
+    basepath = sys.argv[0]
+    #delete file in docs folder
+    for filename in os.listdir('docs'):
+        filepath = os.path.join('docs', filename)
+    #copy static files to docs folder
+    copytree("static","docs")
     #generate pages from markdown files
-    generate_pages_recursive("content", "template.html", "public")
+    
+    generate_pages_recursive("content", "template.html", "docs", basepath)
     
     
     
